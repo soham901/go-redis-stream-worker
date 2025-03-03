2741494c6ee54898951b138a6a3743b0901d9fb8
Soham Sagathiya <sagathiyasoham12345@gmail.com>
2025-03-03 19:28:40 +0530

    add: Implement message production and status retrieval endpoints

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,9 +3,45 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+type Statuses = {
+  [key: string]: {
+    status: string,
+    result: any,
+    timestamp: number,
+    completedAt?: number | null
+  }
+}
+
+const messageStatuses: Statuses = {}
+
+
+app.post('/produce', async (c) => {
+  try {
+    const id = Date.now().toString();
+    const messageId = "message-" + id;
+
+    messageStatuses[id] = {
+      status: 'pending',
+      result: null,
+      timestamp: Date.now()
+    };
+    return c.json({ success: true, messageId, id })
+  } catch (e) {
+    return c.json({ error: e })
+  }
 })
+
+
+app.get('/status/:id', (c) => {
+  const { id } = c.req.param();
+
+  if (messageStatuses[id]) {
+    return c.json(messageStatuses[id]);
+  } else {
+    return c.json({ error: 'Message not found' }, { status: 404 });
+  }
+});
+
 
 serve({
   fetch: app.fetch,
